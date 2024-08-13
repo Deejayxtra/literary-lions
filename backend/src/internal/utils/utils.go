0146cac278ebdb42760e950a54cdf7fccc5af834
femsworld <femsworld@gmail.com>
2024-08-14 02:27:19 +0300

    Added CheckPassword in utils

---
+++ backend/src/internal/utils/utils.go
@@ -0,0 +1,10 @@
+package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func CheckPassword(hashedPassword, plainPassword string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(plainPassword))
+	return err == nil
+}
